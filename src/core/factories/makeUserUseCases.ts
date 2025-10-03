fd67f9b8508666751098396470727a4bd30ef009
Alanakcb <alanacantuaria@gmail.com>
2025-10-03 09:36:06 -0300

    adicionando factories

---
+++ src/core/factories/makeUserUseCases.ts
@@ -0,0 +1,19 @@
+import { IUserRepository } from '../domain/repositories/IUserRepository';
+import { RegisterUser } from '../domain/use-cases/User/RegisterUser';
+import { LoginUser } from '../domain/use-cases/User/LoginUser';
+import { UpdateUser } from '../domain/use-cases/User/UpdateUser';
+import { MockUserRepository } from '../infra/repositories/MockUserRepository';
+
+export function makeUserUseCases() {
+  const userRepository: IUserRepository = new MockUserRepository();
+
+  const registerUser = new RegisterUser(userRepository);
+  const loginUser = new LoginUser(userRepository);
+  const updateUser = new UpdateUser(userRepository);
+
+  return {
+    registerUser,
+    loginUser,
+    updateUser,
+  };
+}
